96cd57f3b8258aad5ca5f67905f205a78e10801e
Daniel_Medina <Medinald@sheridancollege.ca>
2023-02-21 12:26:07 -0500

    Added new class and functionality

---
+++ Midterm/src/midterm/Midterm.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package midterm;
+
+/**
+ * This class +++ Insert class description here +++
+ *
+ * @author Daniel Medina
+ */
+public class Midterm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      // Just a commmit
+    }
+
+}
